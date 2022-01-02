5ec6f943c7b9733b3c446d48ce4c32014e7da0ce
Max Zhuravsky <paperlark@yandex.com>
2022-01-02 19:09:46 +0300

    [master] - show only one row from group

--- src/ui.rs
+++ src/ui.rs
@@ -15,6 +15,7 @@ use crate::dataframe;
 
 struct TableComponent<'a> {
     df: &'a dataframe::DataFrame,
+    grouped_content: Vec<Vec<usize>>, // indices of all occurences in order of occurence
     row_count: usize,
     filter: HashMap<String, dataframe::ColumnValue>,
     selected: usize,
@@ -28,43 +29,57 @@ impl<'a> TableComponent<'a> {
         assert!(!row_counts.is_empty(), "data should have at least one row");
         assert!(row_counts.iter().min() == row_counts.iter().max(), "columns have different number of rows");
 
-        let mut state = widgets::TableState::default();
-        let selected = 0;
-        state.select(Some(selected));
-        TableComponent {
+        let mut table = TableComponent {
             df,
             row_count: row_counts[0],
+            grouped_content: Vec::new(),
             filter: HashMap::new(),
-            selected,
-            state,
-        }
+            selected: 0,
+            state: widgets::TableState::default(),
+        };
+        table.reset_filter();
+        table
     }
 
     fn set_filter(&mut self) {
-        let index = self.selected;
+        let index_in_grouped = self.selected;
+        let index_in_df = self.grouped_content[index_in_grouped][0];
         if self.filter.is_empty() {
             for name in &self.df.group_columns {
                 if let Some(column) = self.df.columns.get(name) {
-                    self.filter.insert(name.clone(), column.values[index].clone());
+                    self.filter.insert(name.clone(), column.values[index_in_df].clone());
                 }
             }
+            self.set_selected(0);
+            self.group_content();
         }
     }
 
     fn reset_filter(&mut self) {
         self.filter.clear();
+        self.set_selected(0);
+        self.group_content();
+    }
+
+    fn set_selected(&mut self, value: usize) {
+        self.selected = value;
+        self.state.select(Some(self.selected));
     }
 
     fn move_selected(&mut self, up: bool) {
-        self.selected = (self.selected + (if up { self.row_count - 1 } else { self.row_count + 1 })) % self.row_count;
-        self.state.select(Some(self.selected));
+        let len = self.grouped_content.len();
+        self.set_selected((self.selected + (if up { len - 1 } else { 1 })) % len);
     }
 
     fn render<B: backend::Backend>(&mut self, f: &mut Frame<B>) {
         let table = widgets::Table::new(self.get_table_contents())
             .header(self.get_table_header())
             .block(widgets::Block::default().borders(widgets::Borders::ALL))
-            .highlight_style(style::Style::default().bg(style::Color::Blue))
+            .highlight_style(
+                style::Style::default()
+                    .bg(style::Color::DarkGray)
+                    .add_modifier(style::Modifier::BOLD),
+            )
             .widths(&[
                 layout::Constraint::Percentage(50),
                 layout::Constraint::Length(30),
@@ -76,20 +91,21 @@ impl<'a> TableComponent<'a> {
         f.render_stateful_widget(table, size, &mut self.state);
     }
 
-    fn get_table_contents<'b>(&self) -> Vec<widgets::Row<'b>> {
+    fn get_table_contents<'b>(&mut self) -> Vec<widgets::Row<'b>> {
         let mut table_contents: Vec<widgets::Row> = Vec::new();
-        let filter_index = self.get_content_filter();
-        for i in filter_index {
-            let mut row_values = Vec::new();
+        for idx in &self.grouped_content {
+            let i = idx[0];
+            let mut row_cells = Vec::new();
             for name in self.get_column_names() {
                 if let Some(c) = self.df.columns.get(name) {
                     let colorize = colorizer::select(c);
                     let v = &c.values[i];
-                    row_values.push(widgets::Cell::from(v.to_string()).style(style::Style::default().fg(colorize(v))));
+                    row_cells.push(widgets::Cell::from(v.to_string()).style(style::Style::default().fg(colorize(v))));
                 }
             }
-            table_contents.push(widgets::Row::new(row_values));
+            table_contents.push(widgets::Row::new(row_cells));
         }
+
         table_contents
     }
 
@@ -121,6 +137,30 @@ impl<'a> TableComponent<'a> {
         } else {
             Box::new(self.df.columns.keys())
         }
+    }
+
+    fn group_content(&mut self) {
+        let filter_index = self.get_content_filter();
+        let mut row_indices: indexmap::IndexMap<Vec<dataframe::ColumnValue>, Vec<usize>> = indexmap::IndexMap::new();
+        for i in filter_index {
+            let mut row_cells = Vec::new();
+            let mut row_values = Vec::new();
+            for name in self.get_column_names() {
+                if let Some(c) = self.df.columns.get(name) {
+                    let colorize = colorizer::select(c);
+                    let v = &c.values[i];
+                    row_values.push(v.clone());
+                    row_cells.push(widgets::Cell::from(v.to_string()).style(style::Style::default().fg(colorize(v))));
+                }
+            }
+            if let Some(row) = row_indices.get_mut(&row_values) {
+                row.push(i);
+            } else {
+                row_indices.insert(row_values, vec![i]);
+            }
+        }
+
+        self.grouped_content = row_indices.into_iter().map(|(_, v)| v).collect();
     }
 }
 
